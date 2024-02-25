dd182453e107fcde3f75e66b214289958ad55652
Admin <scclegion243@gmail.com>
2024-02-25 20:42:04 +0700

    test

---
+++ Models/Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopWeb.Models.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
