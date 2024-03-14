deb1c6574ec7ccd03fbc9f8914883f3028de47cb
Admin <scclegion243@gmail.com>
2024-03-14 07:49:08 +0700

    done fixing bug 14032024

--- Models/ViewModels/LoginVM/LoginViewModel.cs
+++ Models/ViewModels/LoginVM/LoginViewModel.cs
@@ -4,10 +4,10 @@ namespace ShopWeb.Models.ViewModels.LoginVM
 {
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Tên đăng nhập không được để trống!")]
         [Display(Name = "Tên đăng nhập")]
         public string Username { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Mật khẩu không được để trống!")]
         [Display(Name = "Mật khẩu")]
         public string Password { get; set; }
         public string? ReturnUrl { get; set; }
--- Models/ViewModels/RegisterVM/RegisterViewModel.cs
+++ Models/ViewModels/RegisterVM/RegisterViewModel.cs
@@ -4,13 +4,13 @@ namespace ShopWeb.Models.ViewModels.RegisterVM
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Tên đăng nhập không được để trống!")]
         [Display(Name = "Tên đăng nhập")]
         public string Username { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Email không được để trống!")]
         [Display(Name = "Email")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Mật khẩu không được để trống!")]
         [Display(Name = "Mật khẩu")]
         [MinLength(6, ErrorMessage ="Mật khẩu phải có độ dài tối thiểu 6 ký tự!")]
         public string Password { get; set; }
