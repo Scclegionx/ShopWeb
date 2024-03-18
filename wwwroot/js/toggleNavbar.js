fc312831dd4522d6d08d5ec99cee2673d5624546
HA <hoanganhkemosabe@gmail.com>
2024-03-18 09:24:23 +0700

    css cont

---
+++ wwwroot/js/toggleNavbar.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('toggleButton').addEventListener('click', function () {
+    var sidebar = document.getElementById('admin-sidebar');
+    sidebar.style.left = '0px'; // Show the navbar
+});
+
+document.getElementById('hideButton').addEventListener('click', function () {
+    var sidebar = document.getElementById('admin-sidebar');
+    sidebar.style.left = '-250px'; // Hide the navbar
+});
